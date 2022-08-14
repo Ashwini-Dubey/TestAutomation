c9f68512c093478aaecde7e839c58adb3cf3ed94
Ashwini <ak213414@gmail.com>
2022-08-15 05:24:07 +0530

    Negative cases added

--- Test_Automation/API_Automation/API_Automation_Cypress/cypress/integration/POST.spec.js
+++ Test_Automation/API_Automation/API_Automation_Cypress/cypress/integration/POST.spec.js
@@ -7,21 +7,20 @@ describe ('API Automation - POST Method', () =>
     const id = uid()
     const email = `cyapitest${id}@yopmail.com`
     
-        it('User Creation Test', () => {
-            cy.request({
-                method : 'POST',
-                url : 'https://gorest.co.in/public/v1/users',
-                headers : {
-                    'authorization' : 'Bearer ' + accessToken
-                },
-                body : {
-                    "name" : "API Tester",
-                    "gender" : "male",
-                    "email" : email,
-                    "status" : "active"
-
-                }
-            })
+    it('Test to verify that user is created successfully with all the valid request data.', () => {
+        cy.request({
+            method : 'POST',
+            url : 'https://gorest.co.in/public/v1/users',
+            headers : {
+                'authorization' : 'Bearer ' + accessToken
+            },
+            body : {
+                "name" : "API Tester",
+                "gender" : "male",
+                "email" : email,
+                "status" : "active"
+            }
+        })
             .should((Response) => {
             cy.log(JSON.stringify(Response.body))
             expect(Response.status).to.eq(201)
@@ -31,5 +30,29 @@ describe ('API Automation - POST Method', () =>
             expect(Response.body.data.email).to.eq(email)
             })
         })
+
+    it('Test to verify that user is not created with empty email', () => {
+        cy.request({
+            method : 'POST',
+            url : 'https://gorest.co.in/public/v1/users',
+            headers : {
+                'authorization' : 'Bearer ' + accessToken
+            },
+            body : {
+                "name" : "API Tester",
+                "gender" : "male",
+                "email" : "",
+                "status" : "active"
+            }
+        })
+            .should((Response) => {
+            cy.log(JSON.stringify(Response.body))
+            expect(Response.status).to.eq(201)
+            expect(Response.body.data.id).is.exist
+            expect(Response.body.data.name).to.eq('API Tester')
+            expect(Response.body.data.gender).to.eq('male')
+            expect(Response.body.data.email).to.eq(email)
+            })
+        })    
 
 })
