a0f26bd8200d8e6ff72866439710ca05af0d4554
Ashwini <ak213414@gmail.com>
2022-08-15 05:45:06 +0530

    Added tests for POST Method

--- Test_Automation/API_Automation/API_Automation_Cypress/cypress/integration/POST.spec.js
+++ Test_Automation/API_Automation/API_Automation_Cypress/cypress/integration/POST.spec.js
@@ -1,18 +1,27 @@
 /// <reference types="cypress" />
 
+let testData;
+
 describe ('API Automation - POST Method', () =>
 {
-    let accessToken = '0dca8de44b53f262d13eb6e8d85f82c75eb345ff8771b7072959b57b6a81ccd7'
     const uid = () => Cypress._.random(0,1e6)
     const id = uid()
     const email = `cyapitest${id}@yopmail.com`
+
+    before(() => {
+        cy.fixture('user_testdata/user_testdata').then((data) =>
+        {
+            cy.log(data)
+            testData = data
+        })
+    })
     
     it('Test to verify that user is created successfully with all the valid request data.', () => {
         cy.request({
             method : 'POST',
             url : 'https://gorest.co.in/public/v1/users',
             headers : {
-                'authorization' : 'Bearer ' + accessToken
+                'authorization' : 'Bearer ' + testData.accessToken
             },
             body : {
                 "name" : "API Tester",
@@ -29,20 +38,35 @@ describe ('API Automation - POST Method', () =>
             expect(Response.body.data.gender).to.eq('male')
             expect(Response.body.data.email).to.eq(email)
             })
-        })
+    })  
 
-    it('Test to verify that user is not created with empty email', () => {
+})
+
+describe('API Automation - POST Method with fixture' , () => 
+{
+    const uid = () => Cypress._.random(0,1e6)
+    const id = uid()
+    const email = `cyapitest${id}@yopmail.com`
+
+    before(() => {
+        cy.fixture('user_testdata/user_testdata').then((data) =>
+        {
+            cy.log(data)
+            testData = data
+        })
+    })
+    it('Test to verify that user is created successfully with all the valid request data from fixtures file.', () => {
         cy.request({
             method : 'POST',
             url : 'https://gorest.co.in/public/v1/users',
             headers : {
-                'authorization' : 'Bearer ' + accessToken
+                'authorization' : 'Bearer ' + testData.accessToken
             },
             body : {
-                "name" : "API Tester",
-                "gender" : "male",
-                "email" : "",
-                "status" : "active"
+                "name" : testData.name,
+                "gender" : testData.gender,
+                "email" : email,
+                "status" : testData.status
             }
         })
             .should((Response) => {
@@ -53,6 +77,5 @@ describe ('API Automation - POST Method', () =>
             expect(Response.body.data.gender).to.eq('male')
             expect(Response.body.data.email).to.eq(email)
             })
-        })    
-
+    })
 })
